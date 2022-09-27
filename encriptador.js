0915bfca8dea0d19a9878ec235a0d6209a536440
RichardDSantos <richard-ddsantos@hotmail.com>
2022-09-27 18:47:34 -0300

    Criptografia feita com JS
    Código com as chaves da criptografia.

--- encriptador.js
+++ encriptador.js
@@ -1,26 +1,27 @@
-//let blockText = document.querySelector("#mensagem");
+let inputTexto = document.querySelector(".input-texto");
+let outPut = document.querySelector(".container-output");
+
+
+function criptografar(){
+    let texto = inputTexto.value;
     
-let blockText = "Estou trabalhandona barbearia";
+    let resultChaveCripto = texto.replace(/a/g, "a/`´i").replace(/e/g, "¨5$").replace(/i/g, "t*%$").replace(/o/g, "out").replace(/u/g, "ufat");
 
-    let rules = {
-        "a": "ai",
-        "e": "enter",
-        "i": "imes",
-        "o": "ober",
-        "u": "ufat"
-    }
-
-    let result = "";
-    for (let index = 0; index < blockText.length; index++) {
-        const element = blockText.charAt(index);
+    document.querySelector(".container-output").innerHTML = '<textarea readonly class="input-texto">' + resultChaveCripto + '</textarea>' + '<buttom class="encriptar" id="copiar" onclick="copiar()">Copiar</buttom>'
         
-        const existsRules = rules[element];
-        if(typeof existsRules !== "undefined") {
-            result += existsRules;
-        } else {
-            result += element;
-        }
-    }
+}
 
+function descriptografar () {
+    let texto = inputTexto.value;
     
-    console.log(result);
+    let resultChaveDescripto = texto.replace(/ai/g, "a").replace(/exe/g, "e").replace(/imes/g, "i").replace(/out/g, "o").replace(/ufat/g, "u");
+
+    document.querySelector(".container-output").innerHTML = '<textarea readonly class="input-texto">' + resultChaveDescripto  + '</textarea>' + '<buttom class="desencriptar" id="copiar" onclick="copiar()">Copiar</buttom>'
+}
+function copiar(){
+    let textoCop = document.querySelector(".input-texto");
+
+    textoCop.select();
+    document.execCommand('copy');
+    alert("Texto Copiado!");
+}
